25d05a05e0d8bebf0746cb792f8e5259a3a1bd26
NastyaKuvi <30417447+NastyaKuvi@users.noreply.github.com>
2018-02-14 10:52:29 +0300

    rename maps

--- js/form.js
+++ js/form.js
@@ -1,13 +1,13 @@
 'use strict';
 
+var ROOMS = ['room_1', 'rooms_2', 'rooms_100'];
+var GUESTS = ['no_guests', 'guests_3', 'guests_3'];
+
 var TypeToPriceMap = {
   flat: 1000,
   hovel: 0,
   palace: 10000
 };
-
-var RoomsMap = ['room_1', 'rooms_2', 'rooms_100'];
-var GuestsMap = ['no_guests', 'guests_3', 'guests_3'];
 
 var form = document.forms.notice__form;
 var checkInOptions = form.elements.timein;
@@ -33,12 +33,12 @@ houseType.addEventListener('change', function () {
 });
 
 roomNumber.addEventListener('change', function () {
-  capacity.value = GuestsMap[roomNumber.selectedIndex];
+  capacity.value = GUESTS[roomNumber.selectedIndex];
 });
 
 capacity.addEventListener('change', function () {
-  var index = GuestsMap.indexOf(capacity.value);
-  roomNumber.value = RoomsMap[index];
+  var index = GUESTS.indexOf(capacity.value);
+  roomNumber.value = ROOMS[index];
 });
 
 submitBtn.addEventListener('click', function (evt) {
